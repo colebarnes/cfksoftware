c5cbe1875e95b8c5fe1dce71752149902cb017da
Cole Barnes <colebarnes@gmail.com>
2026-07-08 14:25:54 -0500

    Maven Projects
    Initial import of empty maven projects.

---
+++ java/common/src/main/java/org/cfksoftware/common/App.java
@@ -0,0 +1,10 @@
+package org.cfksoftware.common;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
